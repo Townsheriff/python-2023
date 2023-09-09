25f8beb15c78b2f80fce1a627579658766b4a222
Arturs Valenieks <arturs@zumvie.com>
2023-09-09 13:03:59 +0300

    Ran the linter

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 
 
 try:
-  raise Exception("Hello Exception")
+    raise Exception("Hello Exception")
 except Exception as err:
-  print(f'Handled Error message="{err}"')
+    print(f'Handled Error message="{err}"')
 
 
 try:
-  100 / 0
+    100 / 0
 except Exception as err:
-  print(f'Handling division by 0 message="{err}"')
+    print(f'Handling division by 0 message="{err}"')
 
 
 class MyException(Exception):
-  def __init__(self, message, error_code):
-    super().__init__(message)
+    def __init__(self, message, error_code):
+        super().__init__(message)
 
-    self.message = message
-    self.error_code = error_code
-    
+        self.message = message
+        self.error_code = error_code
+
 
 try:
-  raise MyException("my message", 123)
+    raise MyException("my message", 123)
 except MyException as err:
-  print(f'My exception message="{err.message}" error_code={err.error_code}')
+    print(f'My exception message="{err.message}" error_code={err.error_code}')
